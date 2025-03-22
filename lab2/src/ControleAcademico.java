347356748c8b279296c002b1ef140700e0e68f79
sonallycecilia <sonallycecilia11@gmail.com>
2025-03-21 22:19:20 -0300

    Movendo arquivos para lab2 e atualizando ControleAcademico

--- lab2/src/ControleAcademico.java
+++ lab2/src/ControleAcademico.java
@@ -16,15 +16,36 @@ public class ControleAcademico {
                                                 professor1, 
                                                 new Horario(Dia.QUINTA, HoraAula._07_09));
         professor1.vincularDisciplina(disciplina1);
+        Disciplina disciplina2 = new Disciplina(disciplinasOfertadas[3],
+                                                professor1, 
+                                                new Horario(Dia.SEGUNDA, HoraAula._11_13));
+        professor1.vincularDisciplina(disciplina2);
         
         disciplina1.matricularAluno(aluno1);
         aluno1.vincularDisciplina(disciplina1);
+        disciplina1.matricularAluno(aluno2);
+        aluno2.vincularDisciplina(disciplina1);
+        disciplina1.matricularAluno(aluno3);
+        aluno3.vincularDisciplina(disciplina1);
 
+    
+        // respostas
+        System.out.println("\nQuais disciplinas um professor esta ministrando?");
         professor1.printarDisciplinas();
+
+        System.out.println("\nQual o horário de um professor?");
         professor1.printarHorarios();
+
+        System.out.println("\nQuais os alunos de uma dada disciplina?");
         disciplina1.printarAlunosMatriculados();
+        
+        System.out.println("\nQuais as disciplinas de um aluno?");
         aluno1.printarDisciplinas();
+
+        System.out.println("\nQual o horario de um aluno?");
         aluno1.printarHorario();
+
+        System.out.println("\nQual o número de alunos de uma disciplina?");
         System.out.println("Total de alunos da disciplina " + disciplina1.getNome() + " -> " + disciplina1.qtdDeAlunos());
     }
 }
