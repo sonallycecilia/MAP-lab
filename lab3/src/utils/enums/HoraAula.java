14673c7f1996f5d6d56511ee211809d91cbd40e8
Thalubinho <thaleshenriquest@gmail.com>
2025-03-31 11:04:26 -0300

    Adicionado valores aos atributos de HoraAula

--- lab3/src/utils/enums/HoraAula.java
+++ lab3/src/utils/enums/HoraAula.java
@@ -1,9 +1,15 @@
 package utils.enums;
 
 public enum HoraAula {
-    _07_09,
-    _09_11,
-    _11_13,
-    _14_16,
-    _16_18
+    _07_09("07:00-09:00"),
+    _09_11("09:00-11:00"),
+    _11_13("11:00-13:00"),
+    _14_16("14:00-16:00"),
+    _16_18("16:00-18:00");
+
+    private final String hora;
+
+    HoraAula(String s) {
+        this.hora = s;
+    }
 }
