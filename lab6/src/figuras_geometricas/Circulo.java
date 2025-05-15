423334972a76c9a95c336b2e846f2b665689366a
Thalubinho <thaleshenriquest@gmail.com>
2025-05-15 13:48:06 -0300

    Criando Círculo

--- lab6/src/figuras_geometricas/Circulo.java
+++ lab6/src/figuras_geometricas/Circulo.java
@@ -1,4 +1,34 @@
 package figuras_geometricas;
 
 public class Circulo {
+
+    private static Circulo circulo = null;
+    private double raio;
+
+    private Circulo() {
+        
+    }
+    
+    public Circulo getInstance(){
+        if(circulo == null){
+            circulo = new Circulo();
+        }
+        return circulo;
+    }
+
+    public double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+
+    public double getArea() {
+        return Math.PI*(raio * raio);
+    }
+
+    public double getPerimetro() {
+        return 2*Math.PI*raio;
+    }
 }
