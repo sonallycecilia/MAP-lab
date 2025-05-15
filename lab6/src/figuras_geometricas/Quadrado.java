a862f45e87676a249093db3771cc319a4ed90069
Thalubinho <thaleshenriquest@gmail.com>
2025-05-15 13:50:43 -0300

    Criando Quadrado

--- lab6/src/figuras_geometricas/Quadrado.java
+++ lab6/src/figuras_geometricas/Quadrado.java
@@ -1,4 +1,26 @@
 package figuras_geometricas;
 
 public class Quadrado {
+
+    private double lado;
+
+    public Quadrado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getArea() {
+        return lado * lado;
+    }
+
+    public double getPerimetro() {
+        return 4 * lado;
+    }
 }
