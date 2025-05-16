fe994470c27413f5dd90998b552634bbf41749d1
sonallycecilia <sonallycecilia11@gmail.com>
2025-05-16 16:34:45 -0300

    refatorando

--- lab6/src/main/java/org/example/figuras_geometricas/Circulo.java
+++ lab6/src/main/java/org/example/figuras_geometricas/Circulo.java
@@ -6,22 +6,31 @@ public class Circulo {
     private double raio;
 
     private Circulo() {
-        
+
     }
-    
+
+    private Circulo(double raio) {
+        this.raio = raio;
+    }
+
     public Circulo getInstance(){
         if(circulo == null){
             circulo = new Circulo();
         }
         return circulo;
     }
+    //TODO: permitir instanciar circulo com o raio
 
     public double getRaio() {
         return raio;
     }
 
-    public void setRaio(double raio) {
-        this.raio = raio;
+    public void setRaio(double raio) throws IllegalArgumentException {
+        if (raio > 0) {
+            this.raio = raio;
+        } else
+            throw new IllegalArgumentException("O raio deve ser maior que 0");
+
     }
 
     public double getArea() {
---
+++ lab6/src/main/java/org/example/figuras_geometricas/Triangulo.java
@@ -0,0 +1,76 @@
+package figuras_geometricas;
+
+public class Triangulo {
+    private static Triangulo isosceles = null;
+    private static Triangulo equilatero = null;
+    private static Triangulo retangulo = null;
+
+    private double lado1;
+    private double lado2;
+    private double lado3;
+
+    private Triangulo() {
+    }
+
+
+
+    private static boolean ehTriangulo(double lado1, double lado2, double lado3){
+        return (lado1 + lado2 > lado3) &&
+                (lado1 + lado3 > lado2) &&
+                (lado2 + lado3 > lado1);
+    }
+
+    private static boolean ehIsosceles(double lado1, double lado2, double lado3){
+        return ehTriangulo(lado1, lado2, lado3) &&
+                (lado1 == lado2 || lado1 == lado3 || lado2 == lado3);
+    }
+
+    private static boolean ehEquilatero(double lado1, double lado2, double lado3){
+        return ehTriangulo(lado1, lado2, lado3) &&
+                (lado1 == lado2 && lado2 == lado3);
+    }
+
+    private static boolean ehRetangulo(double lado1, double lado2, double lado3){
+        return ehTriangulo(lado1, lado2, lado3) &&
+                (
+                        (Math.pow(lado1, 2) + Math.pow(lado2, 2)) == Math.pow(lado3, 2) ||
+                                (Math.pow(lado1, 2) + Math.pow(lado3, 2) == Math.pow(lado2, 2)) ||
+                                (Math.pow(lado2, 2) + Math.pow(lado3, 2) == Math.pow(lado1, 2))
+                );
+    }
+
+
+
+    public double getLado1() {
+        return lado1;
+    }
+
+    public double getLado2() {
+        return lado2;
+    }
+
+    public double getLado3() {
+        return lado3;
+    }
+
+    private void setLado1(double lado1) throws IllegalArgumentException {
+        if(ehTriangulo(lado1, this.lado2, this.lado3)) {
+            this.lado1 = lado1;
+        } else
+            throw new IllegalArgumentException("O triangulo de lados %.2f, %.2f e %.2f nao eh valido\n".formatted(lado1, lado2, lado3));
+    }
+
+    private void setLado2(double lado2) throws IllegalArgumentException {
+        if(ehTriangulo(this.lado1, lado2, this.lado3)) {
+            this.lado2 = lado2;
+        } else
+            throw new IllegalArgumentException("O triangulo de lados %.2f, %.2f e %.2f nao eh valido\n".formatted(lado1, lado2, lado3));
+    }
+
+    private void setLado3(double lado3) throws IllegalArgumentException {
+        if(ehTriangulo(this.lado1, lado2, lado3)) {
+            this.lado3 = lado3;
+        } else
+            throw new IllegalArgumentException("O triangulo de lados %.2f, %.2f e %.2f nao eh valido".formatted(lado1, lado2, lado3));
+    }
+}
