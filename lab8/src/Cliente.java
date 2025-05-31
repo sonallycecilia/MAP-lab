8663b82f791d4cb2573833f754fe2d2b719d81f0
Thalubinho <thaleshenriquest@gmail.com>
2025-05-31 00:29:57 -0300

    Renomeando ocorrências das classes renomeadas

--- lab8/src/Cliente.java
+++ lab8/src/Cliente.java
@@ -1,10 +1,10 @@
 public class Cliente {
 
 	public static void main(String[] args) {
-		MapParaList<String, Integer> mapLista = new MapParaList<>();
-		ListaParaMap<Integer, String> listaMap = new ListaParaMap<>();
+		MapComoList<String, Integer> mapLista = new MapComoList<>();
+		ListComoMap<Integer, String> listaMap = new ListComoMap<>();
 		
-		// MapParaList - Map disfarçado de List
+		// MapComoList - Map disfarçado de List
 		
 		mapLista.put("Julia", 20);
 		mapLista.put("Maria", 10);
@@ -24,7 +24,7 @@ public class Cliente {
         System.out.println("Após clear, está vazio? " + mapLista.isEmpty());
         System.out.println();
 
-        // ListaParaMap - List disfarçado de Map
+        // ListComoMap - List disfarçado de Map
         listaMap.add("maçã");
         listaMap.add("banana");
         listaMap.add("laranja");
